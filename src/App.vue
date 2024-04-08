2d924ef077138c5fdf2738e1eb3f03248d7b84ad
cactus1998 <s770880qq@gmail.com>
2024-04-08 15:37:00 +0800

    1.移除初始樣式 2.解決pritter換行問題 3.引入tailwind 4.引入pinia

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <mainPage />
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
+<script setup>
+import mainPage from "./components/mainPage.vue";
 </script>
 
 <style>
---
+++ src/components/mainPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <section>
+    <h1>123</h1>
+  </section>
+</template>
+<script setup></script>
+<style scoped></style>
