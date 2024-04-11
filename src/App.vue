09a50849fca1796f442a0398f628fe9d8ca67aea
cactus1998 <s770880qq@gmail.com>
2024-04-11 15:39:20 +0800

    新增：1.輪子照片 2. elPlus 3. 漸層背景

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,7 @@ import mainPage from "./components/mainPage.vue";
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  box-sizing: border-box;
 }
 </style>
--- src/components/mainPage.vue
+++ src/components/mainPage.vue
@@ -1,7 +1,24 @@
 <template>
-  <section>
-    <h1>123</h1>
-  </section>
+  <div class="packground flex h-screen w-screen">
+    <section>
+      <h1>123</h1>
+    </section>
+  </div>
 </template>
 <script setup></script>
-<style scoped></style>
+<style scoped>
+.packground {
+  background: #5a9aac; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to right,
+    #71b280,
+    #5a9aac
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to right,
+    #71b280,
+    #5a9aac
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  opacity: 0.8;
+}
+</style>
